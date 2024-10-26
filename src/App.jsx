d9c5832ed077a3d8bef16352f83b5206eb297c5e
Florencia Navarro <cecine_flor@hotmail.com>
2024-10-25 23:41:24 -0300

    adding quienesSomos´s section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Loading from './components/Loading'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Tienda from './components/Tienda'
 import QuienesSomos from './components/QuienesSomos'
 import Contacto from './components/Contacto'
 
@@ -24,18 +22,13 @@ function App() {
     < >
       {loading ? (<Loading />) : (<>
       <Navbar />
-      
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tienda" element={<Tienda />} />
-        <Route path="/quienes-somos" element={<QuienesSomos />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
+      <Home />
+      <QuienesSomos />
+      <Contacto />
       </>)
       }
     </>
-  );
+  )
 }
 
 export default App
